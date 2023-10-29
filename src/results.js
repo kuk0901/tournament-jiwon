f3413da5a70bcb9b205312f3fc29045e1d71145a
kuk0901 <kukhahyeon@gmail.com>
2023-10-29 20:56:15 +0900

    버튼 사이즈, 결과 페이지 html, js 수정

--- src/results.js
+++ src/results.js
@@ -4,13 +4,17 @@ console.log(JSON.parse(localStorage.getItem("a")));
 
 const totalIndex = JSON.parse(localStorage.getItem("a"));
 
-const containerEl = document.querySelector(".container");
+const sectionEl = document.querySelector(".section");
 
-containerEl.innerHTML = /* html */ `
-<div class="result-picture" style="background-image: url('${
-  Data[totalIndex[0]].src
-}')"></div>
-<div class="sub-title">${Data[totalIndex[0]].title}</div>
+sectionEl.innerHTML = /* html */ `
+  <div class="section-content">
+    <div class="container">
+      <div class="result-picture" style="background-image: url('${
+        Data[totalIndex[0]].src
+      }')"></div>
+      <div class="sub-title">${Data[totalIndex[0]].title}</div>
+    </div>
+  </div>
 `;
 
 localStorage.clear();
